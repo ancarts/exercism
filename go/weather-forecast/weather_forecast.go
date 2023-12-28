4c54e10d053598af8d0ac6753eabbe1a9e2da48b
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2023-12-27 18:49:37 -0800

    Added comments for pacage weather

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather was created to show current weather condition for certain regions in Goblinocus.
 package weather
 
+// CurrentCondition shows a weather condition, including temperature (C), atmospheric pressure, 
+// wind, humidity, precipitation, and cloudiness.
 var CurrentCondition string
+// CurrentLocation is a certain city in Goblinocus.
 var CurrentLocation string
 
+// Forecast returns a string that shows a current weather condition at certain location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
