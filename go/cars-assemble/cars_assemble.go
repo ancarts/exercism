436ae821607fbb3120f58f1c7d914a588773f2a5
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2023-12-28 12:04:30 -0800

    add car assemble

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -1,5 +1,9 @@
 package cars
 
+const minutesInHour = 60
+const singleCarCost = 10000
+const TenCarCost = 95000
+
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
@@ -9,10 +13,10 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float6
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	return int(float64(productionRate)*successRate/float64(minutesInHour)/100)
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	return uint((carsCount/10*TenCarCost) + (carsCount%10*singleCarCost))
 }
