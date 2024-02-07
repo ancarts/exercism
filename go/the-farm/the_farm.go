f75bb1efd635bbff480396aad4dfa0c5702faa3f
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-02-07 12:50:17 -0800

    add farm

---
+++ go/the-farm/the_farm.go
@@ -0,0 +1,25 @@
+package thefarm
+
+// TODO: define the 'DivideFood' function
+func DivideFood(calculator FodderCalculator, numberOfCows int) (float64, error) {
+	panic("")
+}
+// TODO: define the 'ValidateInputAndDivideFood' function
+func ValidateInputAndDivideFood(calculator FodderCalculator, numberOfCows int) (float64, error) {
+	panic("")
+}
+
+// TODO: define the 'ValidateNumberOfCows' function
+
+func ValidateNumberOfCows(numberOfCows int) error {
+panic ("")
+}
+
+// Your first steps could be to read through the tasks, and create
+// these functions with their correct parameter lists and return types.
+// The function body only needs to contain `panic("")`.
+//
+// This will make the tests compile, but they will fail.
+// You can then implement the function logic one by one and see
+// an increasing number of tests passing as you implement more
+// functionality.
