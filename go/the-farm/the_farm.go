883bf4141e213683998e373cd597a1a9e75efe5d
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-02-07 15:31:01 -0800

    solve farm

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -1,18 +1,59 @@
 package thefarm
 
-// TODO: define the 'DivideFood' function
-func DivideFood(calculator FodderCalculator, numberOfCows int) (float64, error) {
-	panic("")
+import (
+	"errors"
+	"fmt"
+)
+
+type InvalidCowsError struct {
+	numberOfCows int
+	message      string
 }
+
+func (i *InvalidCowsError) Error() string {
+
+	return fmt.Sprintf("%d cows are invalid: %s", i.numberOfCows, i.message)
+}
+
+// TODO: define the 'DivideFood' function
+func DivideFood(fodderCalculator FodderCalculator, numberOfCows int) (float64, error) {
+	amount, err := fodderCalculator.FodderAmount(50)
+	if err != nil {
+		return 0, err
+	}
+
+	p, err := fodderCalculator.FatteningFactor()
+	if err != nil {
+		return 0, err
+	}
+
+	foodAmountPerCow := float64(amount) * p / float64(numberOfCows)
+	return foodAmountPerCow, nil
+
+}
+
 // TODO: define the 'ValidateInputAndDivideFood' function
-func ValidateInputAndDivideFood(calculator FodderCalculator, numberOfCows int) (float64, error) {
-	panic("")
+func ValidateInputAndDivideFood(fodderCalculator FodderCalculator, numberOfCows int) (float64, error) {
+	if numberOfCows > 0 {
+		return DivideFood(fodderCalculator, numberOfCows)
+	}
+	return 0, errors.New("invalid number of cows")
 }
 
 // TODO: define the 'ValidateNumberOfCows' function
 
 func ValidateNumberOfCows(numberOfCows int) error {
-panic ("")
+	if numberOfCows == 0 {
+		return &InvalidCowsError{
+			numberOfCows: numberOfCows,
+			message:      "no cows don't need food",
+		}
+	} else if numberOfCows < 0 {
+		return &InvalidCowsError{
+			numberOfCows: numberOfCows,
+			message:      "there are no negative cows"}
+	}
+	return nil
 }
 
 // Your first steps could be to read through the tasks, and create
