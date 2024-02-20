8a6e45f11ea0ca468e8dbb1bed81c3238b86af66
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-02-20 13:05:14 -0800

    solve meteorology

--- go/meteorology/meteorology.go
+++ go/meteorology/meteorology.go
@@ -1,11 +1,17 @@
 package meteorology
 
+import "fmt"
+
 type TemperatureUnit int
 
 const (
 	Celsius    TemperatureUnit = 0
 	Fahrenheit TemperatureUnit = 1
 )
+
+type Stringer interface {
+	String() string
+}
 
 // Add a String method to the TemperatureUnit type
 
@@ -14,7 +20,15 @@ type Temperature struct {
 	unit   TemperatureUnit
 }
 
+func (sc TemperatureUnit) String() string {
+	units := []string{"°C", "°F"}
+	return units[sc]
+}
+
 // Add a String method to the Temperature type
+func (t Temperature) String() string {
+	return fmt.Sprintf("%v %v", t.degree, t.unit)
+}
 
 type SpeedUnit int
 
@@ -24,6 +38,11 @@ const (
 )
 
 // Add a String method to SpeedUnit
+func (s SpeedUnit) String() string {
+	units := []string{"km/h", "mph"}
+	return units[s]
+
+}
 
 type Speed struct {
 	magnitude int
@@ -31,6 +50,9 @@ type Speed struct {
 }
 
 // Add a String method to Speed
+func (s Speed) String() string {
+	return fmt.Sprintf("%v %v", s.magnitude, s.unit)
+}
 
 type MeteorologyData struct {
 	location      string
@@ -41,3 +63,6 @@ type MeteorologyData struct {
 }
 
 // Add a String method to MeteorologyData
+func (md MeteorologyData) String() string {
+	return fmt.Sprintf("%v: %v, Wind %v at %v, %v%% Humidity", md.location, md.temperature, md.windDirection, md.windSpeed, md.humidity)
+}
