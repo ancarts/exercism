99bb7867338e1d299ca317ba449173732a3eca5d
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-02-21 13:33:02 -0800

    solve raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,26 @@
 package raindrops
 
+import "fmt"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	type soundFinder struct {
+		key   int
+		sound string
+	}
+
+	soundFinderSlice := []soundFinder{
+		{key: 3, sound: "Pling"},
+		{key: 5, sound: "Plang"},
+		{key: 7, sound: "Plong"},
+	}
+	var finalSound string
+	for _, v := range soundFinderSlice {
+		if number%v.key == 0 {
+			finalSound += v.sound
+		}
+	}
+	if finalSound == "" {
+		finalSound = fmt.Sprint(number)
+	}
+	return finalSound
 }
