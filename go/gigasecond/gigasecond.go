9a66303b567c9f5bcc545e640d9a4e6d57afdc18
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-02-23 15:24:06 -0800

    solve gigasecond

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,18 +1,16 @@
 // This is a "stub" file.  It's a little start on your solution.
 // It's not a complete solution though; you have to write some code.
 
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package gigasecond contains resolution for gigasecond exercise
 package gigasecond
 
 // import path for the time package from the standard library
 import "time"
 
-// AddGigasecond should have a comment documenting it.
+const secondsInGigasecond = 1000000000
+
+// AddGigasecond shows the date and time one gigasecond after a certain date
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t
+	gigaAge := t.Add(time.Second * secondsInGigasecond)
+	return gigaAge
 }
