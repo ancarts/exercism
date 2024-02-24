fda5a2ec9bf4073be6b751d319f505c8adfbf951
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-02-24 11:37:10 -0800

    solve hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,18 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		errLenghtIsNotEqual := errors.New("lenght is not equal")
+		return 0, errLenghtIsNotEqual
+	}
+
+	count := 0
+	for i := range a {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+	return count, nil
 }
