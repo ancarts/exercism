5292032537f54567c654305929e1149e4b5bc9a0
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-02-24 21:49:04 -0800

    solve scrubble

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,27 @@
 package scrabble
 
+import "strings"
+
 func Score(word string) int {
-	panic("Please implement the Score function")
+	score := 0
+	lettersTovalue := map[string]int{
+		"AEIOULNRST": 1,
+		"DG":         2,
+		"BCMP":       3,
+		"FHVWY":      4,
+		"K":          5,
+		"JX":         8,
+		"QZ":         10,
+	}
+
+	wordUpper := strings.ToUpper(word)
+	for _, v := range wordUpper {
+		for k, j := range lettersTovalue {
+			if strings.ContainsRune(k, v) {
+				score += j
+			}
+
+		}
+	}
+	return score
 }
