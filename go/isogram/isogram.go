adcd062c9361357de2f44848db3101de63159a01
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-03-02 11:16:16 -0800

    solve isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,13 @@
 package isogram
 
+import "strings"
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	toLoweWord := strings.ToLower(word)
+	for _, r := range toLoweWord {
+		if strings.Count(toLoweWord, string(r)) > 1 && string(r) != " " && string(r) != "-" {
+			return false
+		}
+	}
+	return true
 }
