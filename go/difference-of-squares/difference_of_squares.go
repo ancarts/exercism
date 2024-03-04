4bb7c41e7a72c8c5f62008453b0127693bb1bb86
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-03-04 09:00:56 -0800

    solve difference

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,17 @@
 package diffsquares
 
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	sum := (n + 1) * n / 2
+
+	square := sum * sum
+
+	return square
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
