cef67f0339192dfb7dac9143e792f7f86f4a6cf4
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-03-14 13:27:14 -0700

    solve luhn

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,35 @@
 package luhn
 
+import (
+	"strconv"
+	"unicode"
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	idR := []rune(id)
+	if len(id) <= 1 {
+		return false
+	}
+
+	sum := 0
+	n := 0
+
+	for i := len(idR)-1; i >= 0; i-- {
+		num, err := strconv.Atoi((string(idR[i])))
+		if err != nil && !unicode.IsSpace(idR[i]) {
+			return false
+		}
+		if unicode.IsSpace(idR[i]) {
+			continue
+		}
+		if n%2 != 0 {
+			num *= 2
+			if num > 9 {
+				num -= 9
+			}
+		}
+		sum += num
+		n++
+	}
+	return sum%10 == 0&&n>1
 }
