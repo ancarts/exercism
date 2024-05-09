bb921ad164844b67e378d39277a1245e936d2004
Tatiana Bozhenko <tatana@MacBook-Air-4.local>
2024-05-09 15:23:13 -0700

    Add Colors

---
+++ go/resistor-color/resistor_color.go
@@ -0,0 +1,13 @@
+package resistorcolor
+
+// Colors returns the list of all colors.
+func Colors() []string {
+	
+	colors:= []string("Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Grey","White")
+return colors
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	panic("Please implement the ColorCode function")
+}
